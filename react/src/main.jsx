b619c9aae90707e46f0951c706aa0fcaa09ac525
PrachiBarache <prachi.barache@city.ac.uk>
2025-04-17 20:46:04 +0100

    cgohg

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'src/index.css'
-import Portfolio from 'src/App.jsx'
-
+import './index.css'
+import Portfolio from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
