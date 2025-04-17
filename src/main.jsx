fb007c775699f3d8dafefd882ef664156e08cea0
PrachiBarache <prachi.barache@city.ac.uk>
2025-04-17 20:19:53 +0100

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'src/index.css'
-import Portfolio from './src/App.jsx'
+import Portfolio from 'src/App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
